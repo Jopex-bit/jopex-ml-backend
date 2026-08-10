ed8103409f296e1a36aee43f5168f73748ecae55
Jopex-bit <jptjor@gmail.com>
2026-08-10 20:23:37 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -121,6 +121,7 @@ app.use('/api/orders', requireApiKey, require('./routes/orders')(app));
 app.use('/api/items', requireApiKey, require('./routes/items')(app));
 app.use('/api/ads', requireApiKey, require('./routes/ads')(app));
 app.use('/api/competencia', requireApiKey, require('./routes/competencia')(app));
+app.use('/api/diagnostico', requireApiKey, require('./routes/diagnostico')(app));
 
 app.get('/', (req, res) => {
   const tokens = leerTokens();
